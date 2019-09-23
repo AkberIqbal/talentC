1291aea306ec351eed36ae04182c350c2212d27b
M Akber Iqbal <akber.iqbal@gmail.com>
2019-09-23 17:51:20 +1000

    mocha test file updated

--- test/appTest.js
+++ test/appTest.js
@@ -1,20 +1,32 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const assert = require('chai').assert;
+const app = require('../app.js');
 
-const loadAPIDate = require('../script').loadXMLDoc;
+describe('App', function () {
 
-describe('App', function() {
+  it('(1) length of result is atleast 1', function (done) {
+    xhr = new XMLHttpRequest()
+    xhr.open('GET', "https://pb-api.herokuapp.com/bars", true)
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4)
+        console.log("responseText:", xhr.responseText);
+      done();
+    }
+    xhr.send()
+    assert.lengthOf(xhr.responseText, 1, 'length of result is atleast 1')
+  });
 
 
-  it('return an object', function(){
-    var dataLength = 0;
-    var resultFromAPI = loadXMLDoc('http://pb-api.herokuapp.com/bars', function(err, data){
-      if(err){ dataLength =0;}
-      else{
-        dataLength = data.length;
-      }
-    });
-    
-    assert.isAbove( dataLength, 1, 'atleast some data');
-  })
+  it('(2) limit value  is atleast 100', function (done) {
+    xhr = new XMLHttpRequest()
+    xhr.open('GET', "https://pb-api.herokuapp.com/bars", true)
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4)
+        console.log("responseText:", xhr.responseText);
+      done();
+    }
+    xhr.send()
+    assert.isAtLeast(xhr.responseText.limit, 100, 'limit value  is atleast 100')
+  });
 
 });
